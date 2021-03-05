5b9a3375fe84e99726356bf82cd9510a48c967cb
Jonathan <jonathanliu525@gmail.com>
2021-03-05 22:55:58 +1100

    play-js/ObjectIs

---
+++ play-js/1.ES6 and Typescript/1.ES6/17ObjectIs.js
@@ -0,0 +1,7 @@
+//check if two value is equl
+
+const result = 0 == false;
+const result1 = 0 === false;
+const result2 = +0 == -0;
+const result3 = NaN === NaN;
+const result4 = Object.is(NaN, NaN);
