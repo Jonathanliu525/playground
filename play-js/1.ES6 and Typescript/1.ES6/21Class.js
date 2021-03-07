94aae04a53bc668321af037c53e37cbaaff8c2af
Jonathan <jonathanliu525@gmail.com>
2021-03-07 23:44:21 +1100

    play-js/class

---
+++ play-js/1.ES6 and Typescript/1.ES6/21Class.js
@@ -0,0 +1,22 @@
+//old way
+function Person(name) {
+  this.name = name;
+}
+
+Person.prototype.say = function () {
+  console.log(`hi, my name is ${this.name}`);
+};
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  say() {
+    console.log(`hi, my name is ${this.name}`);
+  }
+}
+
+const p = new Person('jon');
+
+p.say();
